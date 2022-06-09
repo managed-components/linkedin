dc150fd6952593b72eb6101070c93912cdeee824
Simona Badoiu <sbadoiu@cloudflare.com>
2022-06-09 12:24:12 +0100

    add basic functionality

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-import { ComponentSettings, Manager } from '@managed-components/types'
+import { ComponentSettings, Manager, MCEvent } from '@managed-components/types'
 
-export default async function (manager: Manager, settings: ComponentSettings) {
-  manager.addEventListener('pageview', event => {
-    // do the things
-  })
+const TRACK_URL = 'https://px.ads.linkedin.com/collect/'
+
+// The pid query parameter, when used alone, is a pageview.
+// Together with a conversionId it becomes a conversion event.
+// conversionIds are typically supplied per 'event'
+const handler = (pid: string) => async (event: MCEvent) => {
+  const payload = {
+    fmt: 'js',
+    v: 2,
+    url: event.client.url.href,
+    time: new Date().valueOf(),
+    pid,
+    ...event.payload,
+  }
+
+  if (Object.keys(payload).length) {
+    const params = new URLSearchParams(payload).toString()
+    event.client.fetch(`${TRACK_URL}?${params}`)
+  }
+}
+
+export default async function (manager: Manager, { pid }: ComponentSettings) {
+  manager.addEventListener('pageview', handler(pid))
+  manager.addEventListener('event', handler(pid))
 }
